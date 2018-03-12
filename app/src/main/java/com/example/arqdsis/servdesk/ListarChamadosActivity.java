f442040524395263b5fea850820b6e7c5e957f4f
arqdsis <arqdsis@mocli10205.animaedu.intranet>
2018-03-12 20:36:12 -0300

    Initial commit

---
+++ app/src/main/java/com/example/arqdsis/servdesk/ListarChamadosActivity.java
@@ -0,0 +1,27 @@
+package com.example.arqdsis.servdesk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class ListarChamadosActivity extends AppCompatActivity {
+
+    public static final String DESCRICAO = "br.usjt.servicedesk.descricao";
+
+    private List<String> lista;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_chamados);
+        Intent origemIntent = getIntent();
+        String chave = origemIntent.getStringExtra(MainActivity.NOME);
+        lista = buscaChamados(chave);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lista);
+        ListView listView = (listView) findViewById(R.id.listView);
+
+    }
+}
